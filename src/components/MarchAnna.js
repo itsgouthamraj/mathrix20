16b04a8ac42dc3cf7acc77e16c09d7776f72e260
Goutham Raj R <itsgouthamraj@gmail.com>
2020-02-26 15:21:24 +0530

    initial

---
+++ src/components/MarchAnna.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const MarchAnna = () => {
+    return (
+        <div className="unbreakable-container">
+            <div className="unbreakable">
+                <span className="un"></span><span class="break">MARCH</span><span class="able"> 9</span>
+            </div>
+            <div className="ks">ANNA UNIVERSITY</div>
+        </div>
+    )
+}
+
+export default MarchAnna;
