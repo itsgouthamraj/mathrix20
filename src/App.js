3ae55634c53a737e1d5a2b5b3e5c67f9cf040175
Goutham Raj R <itsgouthamraj@gmail.com>
2020-02-26 18:29:31 +0530

    cursor

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default class App extends Component {
     render() {
         return (
             <>
+                <div className="expand-bar">
+                    <i className="exp fas fa-expand-arrows-alt"></i>
+                </div>
                 <div className="logo-container">
                     <div>
                         <img src={logo} className="lheader" height={300} />
