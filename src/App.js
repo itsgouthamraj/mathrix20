7647605ba3d551b9f17887245432f79a226fead2
Goutham Raj R <itsgouthamraj@gmail.com>
2020-02-26 16:06:08 +0530

    text

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import MarchAnna from './components/MarchAnna';
 import logo from './assets/mathrix_logo.svg';
 
-export default class App extends Component{
-    render(){
+export default class App extends Component {
+    render() {
         return (
             <>
-            <div className="logo-container">
-                <h1 className="mtext">MATHRIX 20</h1>
-                <img src={logo} className="lheader" height={300}/>
-                
-            </div>
-            <MarchAnna />
+
+                <div className="logo-container">
+                    <div className="mtext">
+                        <div className="mt">MATHRIX</div>
+                    </div>
+                    <img src={logo} className="lheader" height={300} />
+
+                </div>
+                <MarchAnna />
             </>
         )
     }
