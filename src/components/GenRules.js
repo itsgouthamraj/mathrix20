bac5cd75fc7cd5fece740c988a32186b53efa18c
Goutham Raj R <itsgouthamraj@gmail.com>
2020-03-05 12:51:58 +0530

    rules

--- src/components/GenRules.js
+++ src/components/GenRules.js
@@ -13,7 +13,7 @@ const GenRules = (props) => {
             <div className="e-c">
                 <div className="blog-slider events-box">
                     <Row>
-                        <Col xs={12} md={12}>
+                        <Col xs={12} md={12} style={{textAlign:'left'}}>
                             <ul>
                                 <li>Spot Registration will be done between 8.00 am and 9.30 am on 9th March, 2020.</li>
                                 <li>Registration fee is Rs.150 per student.</li>
@@ -22,7 +22,7 @@ const GenRules = (props) => {
                                 <li>Those who applied through online must produce the printout of the transaction receipt.</li>
                                 <li>Participants should wear the MATHRIX 2020 identity card issued at the registration desk.</li>
                                 <li>Online applicants must also collect the MATHRIX 2020 identity card and kit at the registration desk before 9:30 AM</li>
-                                <li>Members of each team should be from the same institution.</li>
+                                <li>Members of each team should be from the same institution. Team members should remain the same till the completion of the particular event.</li>
                                 <li>Participant should be present in the place of event on time. No additional time shall be given for later comers.</li>
                                 <li>Event convener's decision will be final in case of any disputes.</li>
 
